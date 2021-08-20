11cf63ecc8415f605e54df691afb187d19b2bc41
fernandomg <fernando.greco@gmail.com>
2021-08-20 15:07:04 -0300

    update task to receive optional --address param

--- tasks/create-gravatars.js
+++ tasks/create-gravatars.js
@@ -3,11 +3,14 @@ require("@nomiclabs/hardhat-ethers");
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task("create-gravatars", "Adds gravatars for the example contract")
-  .addParam('address', 'Contract deployed address (default: 0x5FbDB2315678afecb367f032d93F642f64180aa3)')
-  .setAction(async ({ address = '0x5FbDB2315678afecb367f032d93F642f64180aa3' }) => {
+  .addOptionalParam(
+    'address',
+    'Contract deployed address (default: 0x5FbDB2315678afecb367f032d93F642f64180aa3)',
+    '0x5FbDB2315678afecb367f032d93F642f64180aa3',
+  )
+  .setAction(async (taskArgs) => {
     // deterministically
-    console.log(address)
-    const Gravity = await ethers.getContractAt('GravatarRegistry', address);
+    const Gravity = await ethers.getContractAt('GravatarRegistry', taskArgs.address);
     await Gravity.functions.createGravatar('Carl', 'https://thegraph.com/img/team/team_04.png');
     await Gravity.functions.createGravatar('Lucas', 'https://thegraph.com/img/team/bw_Lucas.jpg');
 });
