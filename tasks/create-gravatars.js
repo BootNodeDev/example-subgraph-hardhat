c7ba3b8149a61c7675a3bf61cc3484abd5a97ba1
fernandomg <fernando.greco@gmail.com>
2021-08-26 15:15:21 -0300

    use generic examples

--- tasks/create-gravatars.js
+++ tasks/create-gravatars.js
@@ -11,6 +11,6 @@ task("create-gravatars", "Adds gravatars for the example contract")
   .setAction(async (taskArgs) => {
     // deterministically
     const Gravity = await ethers.getContractAt('GravatarRegistry', taskArgs.address);
-    await Gravity.functions.createGravatar('Carl', 'https://thegraph.com/img/team/team_04.png');
-    await Gravity.functions.createGravatar('Lucas', 'https://thegraph.com/img/team/bw_Lucas.jpg');
+    await Gravity.functions.createGravatar('Ship', 'https://images.unsplash.com/photo-1605745341112-85968b19335b?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1951&q=80');
+    await Gravity.functions.createGravatar('Landscape', 'https://images.unsplash.com/photo-1625252595576-d1fbbd522453?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80');
 });
